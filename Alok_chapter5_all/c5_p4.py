20c5bc6ef61010136e0f2a3b778709674db7941b
AlokRajendran <86904083+AlokRajendran@users.noreply.github.com>
2021-11-12 20:39:44 +0530

    Add files via upload

---
+++ Alok_chapter5_all/c5_p4.py
@@ -0,0 +1,10 @@
+# Problem 4: Write a function to compute the number of python files (.py extension) in a specified directory recursively.
+
+import os
+import sys
+
+def count(files):
+  return [file for file in files if '.py' in file]
+files=os.listdir(sys.argv[1])
+pyfiles=count(files)
+print(len(pyfiles))
